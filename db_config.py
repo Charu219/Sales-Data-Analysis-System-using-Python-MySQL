8042804925958458329c1078af37a19caf0b1ee4
Charu219 <charu210907@gmail.com>
2026-05-05 09:26:55 +0530

    Updated DB config for Railway deployment

--- db_config.py
+++ db_config.py
@@ -1,9 +1,29 @@
 import os
+import mysql.connector
 
-host = os.getenv("MYSQLHOST")
-user = os.getenv("MYSQLUSER")
-password = os.getenv("MYSQLPASSWORD")
-database = os.getenv("MYSQLDATABASE")
+def get_connection():
+    return mysql.connector.connect(
+        host=os.getenv("MYSQLHOST"),
+        user=os.getenv("MYSQLUSER"),
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT"))
+    )
+
+
+
+
+
+
+
+
+
+# import os
+
+# host = os.getenv("MYSQLHOST")
+# user = os.getenv("MYSQLUSER")
+# password = os.getenv("MYSQLPASSWORD")
+# database = os.getenv("MYSQLDATABASE")
 
 
 
