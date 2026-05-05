6033d1c1c28d0f991e9e5f88eb14ba4e43f980c4
Charu219 <charu210907@gmail.com>
2026-05-05 10:47:44 +0530

    last change

--- db_config.py
+++ db_config.py
@@ -1,14 +1,32 @@
 import os
 import mysql.connector
+from urllib.parse import urlparse
 
 def get_connection():
+    url = urlparse(os.getenv("MYSQL_URL"))
     return mysql.connector.connect(
-        host=os.getenv("MYSQLHOST"),
-        user=os.getenv("MYSQLUSER"),
-        password=os.getenv("MYSQLPASSWORD"),
-        database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT"))
+        host=url.hostname,
+        user=url.username,
+        password=url.password,
+        database=url.path[1:],
+        port=url.port or 3306
     )
+
+
+
+
+
+# import os
+# import mysql.connector
+
+# def get_connection():
+#     return mysql.connector.connect(
+#         host=os.getenv("MYSQLHOST"),
+#         user=os.getenv("MYSQLUSER"),
+#         password=os.getenv("MYSQLPASSWORD"),
+#         database=os.getenv("MYSQLDATABASE"),
+#         port=int(os.getenv("MYSQLPORT"))
+#     )
 
 
 
