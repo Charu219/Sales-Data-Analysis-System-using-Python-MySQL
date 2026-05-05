7a26a4289127a247cc1b04827d517b132973d2f4
Charu219 <charu210907@gmail.com>
2026-05-05 08:59:00 +0530

    first commit

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def delete_sale(sale_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
 
 
 
--- db_config.py
+++ db_config.py
@@ -1,9 +1,22 @@
-import mysql.connector
+import os
 
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Charu@060905",
-        database="sales_db"
-    )
+host = os.getenv("MYSQLHOST")
+user = os.getenv("MYSQLUSER")
+password = os.getenv("MYSQLPASSWORD")
+database = os.getenv("MYSQLDATABASE")
+
+
+
+
+
+
+
+# import mysql.connector
+
+# def get_connection():
+#     return mysql.connector.connect(
+#         host="localhost",
+#         user="root",
+#         password="Charu@060905",
+#         database="sales_db"
+#     )
